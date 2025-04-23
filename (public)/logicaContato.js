8b9b1f29b59e7ca071cd82951f9a926674f2017a
jose <giraldojose814@gmail.com>
2025-04-23 11:13:46 -0500

    se agrego la funcionalidad para enviar el formulario mediante whattsap

--- (public)/logicaContato.js
+++ (public)/logicaContato.js
@@ -1,33 +1,23 @@
-document.getElementById('formulario-contacto').addEventListener('submit', function(e) {
+document.getElementById('formulario-contacto').addEventListener('submit', function (e) {
     e.preventDefault();
-  
-    // Obtiene los valores de los campos
+    console.log("Formulario enviado correctamente");
+
     const nombre = document.getElementById('nombre').value;
     const telefono = document.getElementById('telefono').value;
-  
-    // Obtiene las cámaras seleccionadas
-    const camaras = [];
-    document.querySelectorAll('input[name="tipo-camara[]"]:checked').forEach((el) => {
-      camaras.push(el.value);
-    });
-  
-    // Validación básica
-    if (camaras.length === 0) {
-      alert('Por favor selecciona al menos un tipo de cámara.');
-      return;
-    }
-  
-    // Arma el mensaje para WhatsApp
-    const mensaje = `Hola, me llamo ${nombre}. Mi número es ${telefono}. Estoy interesado en las siguientes cámaras: ${camaras.join(', ')}.`;
-  
-    
-    const numeroWhatsApp = "573232310187"; //numero del vendedor no usa el + del indicativo
 
-  
-    // Crea el enlace de WhatsApp 
+    const camaras = [];
+    document.querySelectorAll('input[name="tipo-camara[]"]:checked').forEach(function (el) {
+        camaras.push(el.value);
+    });
+
+    if (camaras.length === 0) {
+        alert('Por favor selecciona al menos un tipo de cámara.');
+        return;
+    }
+
+    const mensaje = `Hola, me llamo ${nombre}. Mi número es ${telefono}. Estoy interesado en las siguientes cámaras: ${camaras.join(', ')}.`;
+    const numeroWhatsApp = "573232310187";
     const url = `https://wa.me/${numeroWhatsApp}?text=${encodeURIComponent(mensaje)}`;
-  
-    // Redirecciona al usuario
-    window.location.href = url;
-  });
-  
+
+    window.top.location.href = url;
+});
