be41065c2118806d4125fdcc263832b1d55453fb
jose <giraldojose814@gmail.com>
2025-04-23 12:00:57 -0500

    redireccionamineto a whatsap correctamente

--- (public)/logicaContato.js
+++ (public)/logicaContato.js
@@ -1,23 +1,36 @@
+let formSubmitted = false;
+
 document.getElementById('formulario-contacto').addEventListener('submit', function (e) {
-    e.preventDefault();
-    console.log("Formulario enviado correctamente");
+  e.preventDefault();
+  
+  if (formSubmitted) {
+    return; // Si el formulario ya fue enviado, no hacer nada.
+  }
 
-    const nombre = document.getElementById('nombre').value;
-    const telefono = document.getElementById('telefono').value;
+  formSubmitted = true; // Marca el formulario como enviado
 
-    const camaras = [];
-    document.querySelectorAll('input[name="tipo-camara[]"]:checked').forEach(function (el) {
-        camaras.push(el.value);
-    });
+  console.log("Formulario enviado correctamente");
 
-    if (camaras.length === 0) {
-        alert('Por favor selecciona al menos un tipo de cámara.');
-        return;
-    }
+  const nombre = document.getElementById('nombre').value;
+  const telefono = document.getElementById('telefono').value;
 
-    const mensaje = `Hola, me llamo ${nombre}. Mi número es ${telefono}. Estoy interesado en las siguientes cámaras: ${camaras.join(', ')}.`;
-    const numeroWhatsApp = "573232310187";
-    const url = `https://wa.me/${numeroWhatsApp}?text=${encodeURIComponent(mensaje)}`;
+  const camaras = [];
+  document.querySelectorAll('input[name="tipo-camara[]"]:checked').forEach(function (el) {
+    camaras.push(el.value);
+  });
 
-    window.top.location.href = url;
+  if (camaras.length === 0) {
+    alert('Por favor selecciona al menos un tipo de cámara.');
+    formSubmitted = false; // Permite enviar el formulario nuevamente
+    return;
+  }
+
+  const mensaje = `Hola, me llamo ${nombre}. Mi número es ${telefono}. Estoy interesado en las siguientes cámaras: ${camaras.join(', ')}.`;
+  const numeroWhatsApp = "573232310187";
+  const url = `https://wa.me/${numeroWhatsApp}?text=${encodeURIComponent(mensaje)}`;
+  const url2 = `https://api.whatsapp.com/send?phone=${numeroWhatsApp}&text=${encodeURIComponent(mensaje)}`; //movil
+
+
+  // Redirecciona a WhatsApp
+  window.top.location.href = url,url2;
 });
